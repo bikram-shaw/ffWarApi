4a59fb296f529028e0f1cfa829c6e26c71068593
bikram-shaw <bikramshawbnk@gmail.com>
2021-01-27 22:36:07 +0530

    checksum

--- ffWarUserApi/views/txn.py
+++ ffWarUserApi/views/txn.py
@@ -6,7 +6,7 @@ from ffWarUserApi import checksum
 class ChecksumView(APIView):
     def post(self,request):
         checksum1=checksum.GenerateChecksum(request.data["mid"],request.data["orderid"])
-        print(checksum.VarifyChecksum(checksum1))
+       
 
         return Response(checksum.VarifyChecksum(request.data["mid"],request.data["orderid"],checksum1))
 
