8150067c849e94a5977aafaa44955c000590dd94
bikram-shaw <bikramshawbnk@gmail.com>
2021-01-27 22:30:43 +0530

    checksum

--- ffWarUserApi/checksum.py
+++ ffWarUserApi/checksum.py
@@ -10,6 +10,8 @@ def GenerateChecksum(mid,oid):
     return str(paytmChecksum)
 
 def VarifyChecksum(paytmChecksum):
+     paytmParams["MID"] = mid
+    paytmParams["ORDERID"] = oid
     verifyChecksum = PaytmChecksum.verifySignature(paytmParams, "E%mbk4m&7y6QOXvE",paytmChecksum)
     return str(verifyChecksum)
 
--- ffWarUserApi/views/txn.py
+++ ffWarUserApi/views/txn.py
@@ -7,5 +7,6 @@ class ChecksumView(APIView):
     def post(self,request):
         checksum1=checksum.GenerateChecksum(request.data["mid"],request.data["orderid"])
         print(checksum.VarifyChecksum(checksum1))
-        return Response(checksum1)
+
+        return Response(checksum.VarifyChecksum(request.data["mid"],request.data["orderid"]))
 
