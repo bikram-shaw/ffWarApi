05a7cbba870f8023183d5e1d1bc4fd6847a20d04
bikram-shaw <bikramshawbnk@gmail.com>
2021-01-27 21:42:39 +0530

    checksum

--- ffWarUserApi/checksum.py
+++ ffWarUserApi/checksum.py
@@ -6,11 +6,11 @@ paytmParams = {}
 def GenerateChecksum(mid,oid):
     paytmParams["MID"] = mid
     paytmParams["ORDERID"] = oid
-    paytmChecksum = PaytmChecksum.generateSignature(paytmParams, "&PeD53l7mtZaDnx!")
+    paytmChecksum = PaytmChecksum.generateSignature(paytmParams, "E%mbk4m&7y6QOXvE")
     return str(paytmChecksum)
 
 def VarifyChecksum(paytmChecksum):
-    verifyChecksum = PaytmChecksum.verifySignature(paytmParams, "&PeD53l7mtZaDnx!",paytmChecksum)
+    verifyChecksum = PaytmChecksum.verifySignature(paytmParams, "E%mbk4m&7y6QOXvE",paytmChecksum)
     return str(verifyChecksum)
 
 
