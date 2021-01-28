a3a8945ea5b88de3cd5b3cf08f60866bd6e5e85d
bikram-shaw <bikramshawbnk@gmail.com>
2021-01-28 11:56:21 +0530

    checksum

--- ffWarUserApi/views/txn.py
+++ ffWarUserApi/views/txn.py
@@ -2,13 +2,14 @@ from rest_framework import  permissions,decorators
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ffWarUserApi import checksum
+import random
 @decorators.permission_classes([permissions.AllowAny])
 class ChecksumView(APIView):
     def post(self,request):
         checksum1=checksum.GenerateChecksum(request.data["mid"],request.data["orderid"])
        
-        
-        return Response(checksum1)
+        res={"oid":random.random(),"checksum":checksum1}
+        return Response(res)
 @decorators.permission_classes([permissions.AllowAny])
 class callback(APIView):
     def post(self,request):
